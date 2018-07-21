d5aebe991581be77f07f92615e53d515c938f839
Daniel Sel <daniel-sel@hotmail.com>
2018-07-21 23:05:21 +0200

    broken

--- contracts/RideSharing.sol
+++ contracts/RideSharing.sol
@@ -7,10 +7,22 @@ contract RideSharing {
     address owner;
     uint16 rideSequence;
 
-    struct rideInfo {
-        string checkinMessage;
-        string lastPop;
+    struct checkinMessage {
+        int32 start_lat; 
+        int32 start_lng; 
+        uint32 startTimestamp; 
+        uint16 price; 
+        address driver;
         address passenger;
+    }
+
+    struct pop {
+        uint16 rideId; 
+        uint16 km;
+    }
+
+    struct signature {
+        // ...
     }
 
     constructor() public {
@@ -18,7 +30,15 @@ contract RideSharing {
         rideSequence = 1;
     }
 
-    function newRide(int32 start_lat, int32 start_lng, uint32 startTimestamp, uint16 price, address driver, address passenger) external {
+    /* Only for rides where driver offers trusted gps source! (RideSharing with GPS tracker, Uber, Taxi, Train, Airtaxi, ...)
+        1.) Driver publishes ride offer on AWS
+        2.) Passenger requests to join ride by copying info from AWS service and signing it with Ethereum Private Key
+        3.) Driver accepts Passenger by calling this method with the signed "join request" from the Passenger */
+    event RideAccepted(uint16 rideId, address passenger);    
+    function newRide(int32 start_lat, int32 start_lng, uint32 startTimestamp, uint16 price, address driver, address passenger, signature sig) external {
+        // TODO Verify signature
+        // require...
+
 
     }
 
