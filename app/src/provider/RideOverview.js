cfa998f5baf69755d38a7c304ad5ee36483c1875
David Schopf <david.schopf@jambit.com>
2018-07-22 00:12:01 +0200

    Passenger confirmation logic

--- app/src/provider/RideOverview.js
+++ app/src/provider/RideOverview.js
@@ -1,7 +1,60 @@
 import React, {Component} from "react";
+import io from 'socket.io-client';
+import CheckMarkIcon from "../icons/CheckmarkIcon";
+
+const NoPassengers = props => <div>No passengers yet</div>;
+
+
+const RedX = props => <span className="red">X</span>;
+
+export class PassengerToConfirm extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.confirmPassenger = this.confirmPassenger.bind(this)
+    }
+
+    confirmPassenger() {
+        console.log("confirm passenger", this.props.passenger.name);
+    }
+
+    render() {
+        return <div>{this.props.passenger.name} <CheckMarkIcon onClick={this.confirmPassenger}/> <RedX/></div>;
+    }
+}
+
+export class PassengersWithConfirmation extends React.Component {
+
+    render() {
+        return <div>
+            {this.props.passengers.map(passenger => <PassengerToConfirm key={passenger.id} passenger={passenger}/>)}
+        </div>
+    }
+}
 
 export class RideOverview extends Component {
 
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            passengers: []
+        }
+    }
+
+    componentDidMount() {
+        // Subscribe to websocket
+
+        setTimeout(() => {
+            this.setState({
+                passengers: [
+                    {id: "xcxc", name: "Alexander K."}
+                ]
+            });
+
+        }, 4000);
+
+    }
 
     render() {
 
@@ -9,7 +62,8 @@ export class RideOverview extends Component {
             <h3 className="font-accent">your ride is online</h3>
             <h3>Passengers</h3>
             <hr/>
-            <div>No passengers yet</div>
+            {this.state.passengers.length > 0 ? <PassengersWithConfirmation passengers={this.state.passengers}/> :
+                <NoPassengers/>}
         </div>
     }
 }
