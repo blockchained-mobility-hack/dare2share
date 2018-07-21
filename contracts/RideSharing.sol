7caeae8813356adc605c9785f1485c5f40881547
Daniel Sel <daniel-sel@hotmail.com>
2018-07-21 17:31:51 +0200

    smart contract start

--- contracts/RideSharing.sol
+++ contracts/RideSharing.sol
@@ -1,8 +1,39 @@
-pragma solidity ^0.4.17;
+pragma solidity ^0.4.24;
+pragma experimental ABIEncoderV2;
 
 
 contract RideSharing {
 
+    address owner;
+    uint16 rideSequence;
 
+    struct rideInfo {
+        string checkinMessage;
+        string lastPop;
+        address passenger;
+    }
+
+    constructor() public {
+        owner = msg.sender;
+        rideSequence = 1;
+    }
+
+    function newRide(int32 start_lat, int32 start_lng, uint32 startTimestamp, uint16 price, address driver, address passenger) external {
+
+    }
+
+    function cashout(rideInfo[]) external {
+        // pop contains rideId, timestamp, lat, lng, km, address
+    }
+
+    function getBalance(address user) {
+        if(user == msg.sender) {
+
+        }
+
+        else {
+
+        }
+    }
 
 }
