ca40c390a18ec54d3df486d382b7d3d1044eb6d4
David Schopf <david.schopf@jambit.com>
2018-07-21 19:25:46 +0200

    colors, header, footer

--- app/src/scaffold/AppHeader.js
+++ app/src/scaffold/AppHeader.js
@@ -1,9 +1,23 @@
 import React, {Component} from "react";
 
+
+
+const MenuIcon = props => (
+    <svg width={29} height={22} {...props}>
+        <g fill="#F67264" fillRule="nonzero">
+            <rect width={29} height={4} rx={2} />
+            <rect y={9} width={29} height={4} rx={2} />
+            <rect y={18} width={29} height={4} rx={2} />
+        </g>
+    </svg>
+);
+
+
+
 export class AppHeader extends Component {
 
     render() {
-        return <div className="App-header">Dare2Share</div>
+        return <div className="App-header"><MenuIcon className="fl"/> Dare2Share</div>
     }
 
 }
