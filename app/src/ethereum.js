140d1703accb150280bcd5ba35e6190809503fcc
David Schopf <david.schopf@jambit.com>
2018-07-22 14:04:37 +0200

    new smart contract address

--- app/src/ethereum.js
+++ app/src/ethereum.js
@@ -1,6 +1,6 @@
 
 
-export const SmartContractAddress = "0xcf3985f5f97c24eb51b48b5043421d060f1f19eb";
+export const SmartContractAddress = "0x6c7c3c62a24d06155e6447de293322629df23086";
 
 
 export const DriverAddress = "0x6dC1675eE2122c69c3d5FbCE458D9cfAe03C52A0";
--- truffle.js
+++ truffle.js
@@ -3,7 +3,7 @@ module.exports = {
   // for more about customizing your Truffle configuration!
   networks: {
     development: {
-      host: "127.0.0.1",
+      host: "172.27.64.176",
       port: 7545,
       network_id: "*" // Match any network id
     }
