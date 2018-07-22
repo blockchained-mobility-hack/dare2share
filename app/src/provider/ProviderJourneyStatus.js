f83ba55cc3662472f5a694b7727a0c5443c924fe
David Schopf <david.schopf@jambit.com>
2018-07-22 10:24:04 +0200

    driver receives passenger check in (timeout mock)

--- app/src/provider/ProviderJourneyStatus.js
+++ app/src/provider/ProviderJourneyStatus.js
@@ -1,18 +1,34 @@
 import React, {Component} from "react";
 import {PassengerList, PASSENGERS} from "./PassengerList";
-import {AppFooter} from "../scaffold/AppFooter";
-import {Link} from "react-router-dom";
 
 export class ProviderJourneyStatus extends Component {
+
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            passengers: PASSENGERS
+        }
+    }
+
+    componentDidMount() {
+        // Register on websocket
+
+        setTimeout(() => {
+
+            const passenger = PASSENGERS[0];
+            passenger.status = "active";
+            this.setState({
+                passengers: [passenger]
+            })
+        }, 2000);
+    }
 
     render() {
         return <div>
             <h3>Munich &rarr; Berlin</h3>
-            <h3>Passengers</h3>
-            <PassengerList passengers={PASSENGERS}/>
-            <AppFooter>
-                <Link to="/wallet">Collect payment</Link>
-            </AppFooter>
+            <h3 className="font-accent">Your passengers</h3>
+            <PassengerList passengers={this.state.passengers}/>
         </div>
     }
 
