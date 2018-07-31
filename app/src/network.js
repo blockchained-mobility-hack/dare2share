05cdde26f40d6cc25dbd8080b22a3f09ad7de1d0
moralesl <luis.morales@gmx.de>
2018-07-31 23:44:40 +0200

    Reset socket address to localhost

--- app/src/network.js
+++ app/src/network.js
@@ -1,32 +1,31 @@
+export const SocketAdress = "http://localhost:4200";
 
-
-
-// export const SocketAdress = "http://localhost:4200";
-export const SocketAdress = "http://172.27.64.176:4200";
-
-export const sendPassengerRequest = data => fetch(SocketAdress + '/join', {
-    method: 'POST',
+export const sendPassengerRequest = data =>
+  fetch(SocketAdress + "/join", {
+    method: "POST",
     headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
+      Accept: "application/json",
+      "Content-Type": "application/json"
     },
     body: JSON.stringify(data)
-});
+  });
 
-export const sendDriverConfirmation= data => fetch(SocketAdress + '/join', {
-    method: 'POST',
+export const sendDriverConfirmation = data =>
+  fetch(SocketAdress + "/join", {
+    method: "POST",
     headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
+      Accept: "application/json",
+      "Content-Type": "application/json"
     },
     body: JSON.stringify(data)
-});
+  });
 
-export const checkInPassenger = data => fetch(SocketAdress + '/check-in', {
-    method: 'POST',
+export const checkInPassenger = data =>
+  fetch(SocketAdress + "/check-in", {
+    method: "POST",
     headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
+      Accept: "application/json",
+      "Content-Type": "application/json"
     },
     body: JSON.stringify(data)
-});
+  });
