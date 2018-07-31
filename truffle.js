e1132b3f474d4818dcc3ce315e3bb97caf8067b9
moralesl <luis.morales@gmx.de>
2018-07-31 23:22:38 +0200

    Reset development host address

--- truffle.js
+++ truffle.js
@@ -3,7 +3,7 @@ module.exports = {
   // for more about customizing your Truffle configuration!
   networks: {
     development: {
-      host: "172.27.64.176",
+      host: "127.0.0.1",
       port: 7545,
       network_id: "*" // Match any network id
     }
